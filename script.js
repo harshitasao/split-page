79e5d89f2eef5fbb0812ec281ac48e144eabeb10
harshitasao <84518563+harshitasao@users.noreply.github.com>
2021-12-09 16:31:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const container = document.querySelector('.container');
+var left = document.querySelector('.left');
+var right = document.querySelector('.right');
+
+left.addEventListener('mouseenter', () => container.classList.add('hover-left'));
+left.addEventListener('mouseleave', () => container.classList.remove('hover-left'));
+
+right.addEventListener('mouseenter', () => container.classList.add('hover-right'));
+right.addEventListener('mouseleave', () => container.classList.remove('hover-right'));
